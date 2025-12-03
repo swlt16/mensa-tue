7aab6376cdbf09e4da86152e74b8a22d226ccca1
Sebastian Walter <sebastian@sebastian-walter.net>
2025-12-03 15:32:51 +0100

    add spoiler generator script

---
+++ mensa-spoiler.py
@@ -0,0 +1,32 @@
+import sys
+
+class Spoiler:
+    # fancy data model
+    begin = 0
+    length = 0
+    def __str__(self):
+        return f"{self.begin}:{self.length}:SPOILER"
+
+# read stdin
+input = sys.stdin.read()
+
+
+# search for spoiler tags
+offset = 0
+while input.find("<spoiler>", offset) != -1:
+    # spoiler repr
+    spoiler = Spoiler()
+    # start pos of spoiler tag
+    pos = input.find("<spoiler>", offset)
+    spoiler.begin = pos
+    # remove spoiler begin tag
+    input = input.replace("<spoiler>", "", 1)
+    # find closing tag
+    pos = input.find("</spoiler>", offset)
+    spoiler.length = pos - spoiler.begin
+    # remove spoiler end tag
+    input = input.replace("</spoiler>", "", 1)
+    # prepare next round
+    offset = pos
+    # debug output
+    print(spoiler)
